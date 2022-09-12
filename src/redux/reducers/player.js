75baf193871d1fef0de958a167b63f4d29babb15
FabioSC <fabioscarvalh@yahoo.com.br>
2022-09-12 19:08:31 -0300

    Requisito 9

--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -1,9 +1,9 @@
-import { NEW_PLAYER } from '../actions';
+import { NEW_PLAYER, SUM_SCORE } from '../actions';
 
 const INITIAL_STATE = {
   name: 'nome-da-pessoa',
   assertions: 'número-de-acertos',
-  score: 'pontuação',
+  score: 0,
   gravatarEmail: 'email-da-pessoa',
 
 };
@@ -12,6 +12,8 @@ export default function player(state = INITIAL_STATE, action) {
   switch (action.type) {
   case NEW_PLAYER:
     return { ...state, name: action.payload.name, gravatarEmail: action.payload.email };
+  case SUM_SCORE:
+    return { ...state, score: state.score + action.payload };
   default:
     return state;
   }
