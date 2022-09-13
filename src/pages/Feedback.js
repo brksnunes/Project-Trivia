4163bd70e0aa96e2722706ebb5079c003c17d1fd
Rodrigo Nunes <brksnunes@protonmail.com>
2022-09-13 19:26:20 -0300

    style: :lipstick: Added styling for the Feedback page

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
 import { restart } from '../redux/actions';
-import Header from '../components/Header';
+import logo from '../assets/login_logo.png';
+// import Header from '../components/Header';
 
 class Feedback extends React.Component {
   newGame = () => {
@@ -18,41 +20,57 @@ class Feedback extends React.Component {
   };
 
   render() {
-    const { total, question } = this.props;
+    const { total, question, info } = this.props;
+    const { gravatarEmail } = info;
+    const converted = md5(gravatarEmail).toString();
     // const total = 0; // variável para receber pontuação total
     // const question = 0; // variável para receber total de perguntas certas
     const MINIMUM_SCORE = 2;
     return (
       <div>
-        <Header />
-        <h1
-          data-testid="feedback-total-score"
-        >
-          {`${total}`}
-        </h1>
-        <h1
-          data-testid="feedback-total-question"
-        >
-          {`${question}`}
-        </h1>
-        { question <= MINIMUM_SCORE
-        && <h2 data-testid="feedback-text">Could be better...</h2> }
-        { question > MINIMUM_SCORE
-        && <h2 data-testid="feedback-text">Well Done!</h2> }
-        <button
-          type="button"
-          onClick={ this.newGame }
-          data-testid="btn-play-again"
-        >
-          Play Again
-        </button>
-        <button
-          type="button"
-          onClick={ this.goToRanking }
-          data-testid="btn-ranking"
-        >
-          Ranking
-        </button>
+        {/* <Header /> */}
+        <img src={ logo } className="feedback-logo" alt="logo" />
+        <div className="feedback-container">
+          <img
+            src={ `https://www.gravatar.com/avatar/${converted}` }
+            alt={ gravatarEmail }
+            data-testid="header-profile-picture"
+            className="player-picture"
+          />
+          <div className="feedback-text">
+            { question <= MINIMUM_SCORE
+          && <h1 data-testid="feedback-text">Could be better...</h1> }
+            { question > MINIMUM_SCORE
+          && <h1 data-testid="feedback-text">Well Done!</h1> }
+            <p
+              data-testid="feedback-total-question"
+            >
+              {`Você acertou ${question} questões!`}
+            </p>
+            <p
+              data-testid="feedback-total-score"
+            >
+              {`Um total de ${total} pontos`}
+            </p>
+          </div>
+
+        </div>
+        <div className="feedback-buttons">
+          <button
+            type="button"
+            onClick={ this.goToRanking }
+            data-testid="btn-ranking"
+          >
+            Ranking
+          </button>
+          <button
+            type="button"
+            onClick={ this.newGame }
+            data-testid="btn-play-again"
+          >
+            Play Again
+          </button>
+        </div>
       </div>
     );
   }
@@ -61,11 +79,13 @@ class Feedback extends React.Component {
 const mapStateToProps = (state) => ({
   total: state.player.score,
   question: state.player.assertions,
+  info: state.player,
 });
 
 Feedback.propTypes = {
   total: PropTypes.number.isRequired,
   question: PropTypes.number.isRequired,
+  info: PropTypes.objectOf(PropTypes.any.isRequired).isRequired,
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
